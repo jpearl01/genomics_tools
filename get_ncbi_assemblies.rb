c7ec2250d5eb837025da1a797cdfc8d5def8753f
Josh Earl <joshearl1@hotmail.com>
2017-09-29 14:39:34 -0400

    added a new output file when getting ncbi assemblies for record information for each strain'

--- get_ncbi_assemblies.rb
+++ get_ncbi_assemblies.rb
@@ -2,13 +2,20 @@
 
 require 'open-uri'
 
+#Usage: ruby get_ncbi_assemblies.rb "Moraxella catarrhalis"
+#Creates a 'genomes' folder and puts all fasta assemblies there, named by 'strain'
+#Also creates a file with record information for each strain 'strain_records.tsv'
+
 species = ARGV[0].to_s
 puts "Searching for species: "+ species
 
 Dir.mkdir("genomes") unless File.exists?("genomes")
 
+out = File.open('strain_records.tsv', 'w')
+
 open('ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt').each do |line|
   if /#{species}/.match(line)
+    out.puts(line)
     a = line.split("\t")
     ftp = a[-3]
     strain = a[8].split("=")[-1]
